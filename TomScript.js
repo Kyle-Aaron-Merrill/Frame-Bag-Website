7a96c37ae1e900c957075b03440f530bba2312b9
Kyle Merrill <74798513+KyleTehWhale@users.noreply.github.com>
2022-07-25 17:17:51 -0400

    Added containers and about info
    Improved some animations and added some containers with text. 11:50am - 4:00pm

--- TomScript.js
+++ TomScript.js
@@ -1,15 +1,21 @@
 let buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', function(){
-        if(this.classList.contains('active')){
-            this.classList.remove('active');
-        }
-        else{
+        if(this.classList.contains('burger-button') && !this.classList.contains('active')){
             this.classList.add('active');
-        }
-        if(this.classList.contains('burger-button')){
             document.getElementById('dropdown').classList.add('active');
             document.getElementById('site-banner').classList.add('active');
+            document.getElementById('site-container').style.display = 'none';
+        }
+        else{
+            this.classList.remove('active');
+            document.getElementById('dropdown').classList.remove('active');
+            document.getElementById('site-banner').classList.remove('active');
+            document.getElementById('site-container').style.display = 'block';
         }
     })
 });
+
+function imageSlider(){
+    let 
+}
