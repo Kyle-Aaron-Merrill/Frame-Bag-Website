96ac7670fcf5aaaabc23c39002cceceec351cf57
Kyle Merrill <74798513+KyleTehWhale@users.noreply.github.com>
2022-07-26 00:49:06 -0400

    Added more photos and animations
    Added some @keyframes, Made the website mobile compatible, and added some pictures for more detail.
    8:30pm-12:00am

--- TomScript.js
+++ TomScript.js
@@ -1,3 +1,8 @@
+setTimeout(function(){
+    document.body.className="";
+},500);
+
+
 let buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', function(){
@@ -5,13 +10,15 @@ buttons.forEach(button => {
             this.classList.add('active');
             document.getElementById('dropdown').classList.add('active');
             document.getElementById('site-banner').classList.add('active');
-            document.getElementById('site-container').style.display = 'none';
+            document.getElementById('site-nav').classList.add('active');
+            document.getElementById('site-container').classList.add('active');
         }
         else{
             this.classList.remove('active');
             document.getElementById('dropdown').classList.remove('active');
             document.getElementById('site-banner').classList.remove('active');
-            document.getElementById('site-container').style.display = 'block';
+            document.getElementById('site-nav').classList.remove('active');
+            document.getElementById('site-container').classList.remove('active');
         }
     })
 });
