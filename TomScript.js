840b3345f1f434bc3796c882b28831d0c96ecf93
Kyle Merrill <74798513+KyleTehWhale@users.noreply.github.com>
2022-07-26 17:40:51 -0400

    Added a image swipper and improved Mobile layout.
    Image swipper has bubbles and arrows that allow users to cycle images. Mobile styling is now more responsive.
    2pm-3pm

--- TomScript.js
+++ TomScript.js
@@ -23,6 +23,32 @@ buttons.forEach(button => {
     })
 });
 
-function imageSlider(){
-    let 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let numbers = document.getElementsByClassName("numbertext");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    numbers[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  numbers[slideIndex-1].style.display = 'block';
+  dots[slideIndex-1].className += " active";
 }
